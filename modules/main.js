f7c055d162fcb174aeef64fc827433417d66bf22
soaud arziki <arziki.souad@gmail.com>
2025-09-25 20:01:35 +0100

    refactorisation complète du code, ajout du timer globale du quiz et fixe des erreurs

--- modules/main.js
+++ modules/main.js
@@ -1,20 +1,98 @@
-import { fetchData } from "./quiz.js";
-import { switchScreen } from "./UI.js";
+import {
+  fetchData,
+  showQuestions,
+  validateAnswer,
+  updateQuizHistorique,
+  handleNextStep,
+  quizState,
+  timerGlobale,
+  feedBack,
+  timerG,
+  stopTimer,
+  totalSeconds
+} from "./quiz.js";
+import { switchScreen, showResult } from "./UI.js";
 
+quizState.score = 0;
+quizState.currentIndex = 0;
+quizState.quizHistory = [];
+
+const startScreen = document.getElementById("startScreen");
 const categories = document.querySelectorAll(".cat");
 const categorieScreen = document.getElementById("categorieScreen");
 const questionScreen = document.getElementById("questionScreen");
-let currentIndex = 0;
+const reponsesDiv = document.getElementById("reponses");
+const resultScreen = document.getElementById("resultScreen");
+const timeGlobale = document.getElementById("timeGlobale");
+const nbrQuestions = document.getElementById("nbrQuestions");
+const feedback = document.querySelector("#feedBack");
+const recommancerBtn = document.querySelector("#recommancerBtn");
+
+export let questionsData = [];
 
 categories.forEach((cat) => {
-    cat.addEventListener("click", async () =>{
-        switchScreen(categorieScreen, questionScreen);
-        const categorie = cat.dataset.categorie;
-        const dataCategorie = await fetchData(categorie);
-        let questionsData = dataCategorie.questions;
-        if(dataCategorie){
-            showQuestions(questionsData, currentIndex);
-        }
+  cat.addEventListener("click", async () => {
+    switchScreen(categorieScreen, questionScreen);
+    timerGlobale();
+    const categorie = cat.dataset.categorie;
+    const dataCategorie = await fetchData(categorie);
+    questionsData = dataCategorie.questions;
+    nbrQuestions.textContent = questionsData.length;
+    quizState.quizHistory.push({
+      //   username: usernameValue,
+      categorie: categorie,
+      reponses: [],
+      date: new Date().toLocaleString(),
+      score: quizState.score,
     });
+    if (dataCategorie) {
+      showQuestions(questionsData, quizState.currentIndex);
+    }
+  });
 });
 
+nextBtn.addEventListener("click", () => {
+  console.log(totalSeconds);
+  const currentQuestionData = questionsData[quizState.currentIndex];
+  const { userChoices, isCorrect, correctAnswers } = validateAnswer(
+    currentQuestionData,
+    reponsesDiv
+  );
+
+  if (isCorrect) quizState.score++;
+
+  updateQuizHistorique(
+    quizState.quizHistory,
+    currentQuestionData,
+    userChoices,
+    correctAnswers
+  );
+  quizState.currentIndex = handleNextStep(
+    quizState.currentIndex,
+    quizState.quizHistory,
+    quizState.score
+  );
+});
+
+//à revoir la logic de locale storage
+submitBtn.addEventListener("click", () => {
+  stopTimer(timerG);
+  switchScreen(questionScreen, resultScreen);
+  // localStorage.setItem("historique", JSON.stringify(quizHistorique));
+
+  let dernierQuiz = quizState.quizHistory[quizState.quizHistory.length - 1];
+  quizState.quizHistory.push(dernierQuiz);
+  const totalQuest = questionsData.length;
+  feedback.innerHTML = feedBack(dernierQuiz.score, totalQuest);
+  showResult(dernierQuiz, totalSeconds);
+});
+
+recommancerBtn.addEventListener("click", () => {
+  switchScreen(resultScreen, startScreen);
+
+  //   usernameInput.value = localStorage.getItem("username");
+  quizState.currentIndex = 0;
+  quizState.score = 0;
+  nextBtn.style.display = "block";
+  submitBtn.style.display = "none";
+});
--- modules/ui.js
+++ modules/ui.js
@@ -1,21 +1,68 @@
+import { questionsData } from "./main.js";
+
+const timeGlob = document.getElementById("timeGlob");
+const finalResult = document.querySelector("#finalResult");
+const scoreFinale = document.getElementById("scoreFinale");
+const total = document.getElementById("totalQuestions");
 
 export function switchScreen(firstScreen, secondeScreen) {
-    firstScreen.classList.remove("active");
-    secondeScreen.classList.add("active");
+  firstScreen.classList.remove("active");
+  secondeScreen.classList.add("active");
 }
 
 //fonction qui cree les éléments d'une reponse (input, label)
-export function createAnswerInput(rep, i, questionType){
-    const input = document.createElement("input");
-    input.value = rep;
-    input.classList.add("ResInput");
-    input.id = `answer-${i}`;
-    input.setAttribute("data-resIndex", i);
-    input.type = questionType == "multiple" ? "checkbox" : "radio";
-    const label = document.createElement("label");
-    label.textContent = rep;
-    label.classList.add("answer");
-    label.appendChild(input);
+export function createAnswerInput(rep, i, questionType) {
+  const input = document.createElement("input");
+  input.value = rep;
+  input.classList.add("ResInput");
+  input.id = `answer-${i}`;
+  input.setAttribute("data-resIndex", i);
+  input.type = questionType == "multiple" ? "checkbox" : "radio";
+  const label = document.createElement("label");
+  label.textContent = rep;
+  label.classList.add("answer");
+  label.appendChild(input);
 
-    return {input, label};
+  return { input, label };
+}
+
+//fonction
+export function handleAnswerSelection(input, label, nextBtn) {
+  if (input.type === "radio") {
+    document
+      .querySelectorAll(".answer")
+      .forEach((label) => label.classList.remove("selected"));
+    label.classList.add("selected");
+  } else {
+    label.classList.toggle("selected");
+  }
+  nextBtn.disabled = false;
+}
+
+export function showResult(quiz, totalSeconds) {
+  timeGlob.textContent = `${totalSeconds} seconds `;
+  scoreFinale.textContent = quiz.score;
+  total.textContent = questionsData.length;
+  finalResult.innerHTML = "";
+  quiz.reponses.forEach((reponse, i) => {
+    const divResult = document.createElement("div");
+    divResult.classList.add("divResult");
+    const questionResult = document.createElement("p");
+    questionResult.innerHTML = `<span class="questRes">Question ${
+      i + 1
+    }:</span> ${reponse.quest}`;
+
+    const divReponsesResult = document.createElement("div");
+    divReponsesResult.innerHTML = `<strong>Votre réponse :</strong> ${reponse.reponseChosie}`;
+    divReponsesResult.id = "divReponsesResultId";
+
+    const repCorretResult = document.createElement("p");
+    repCorretResult.innerHTML = `<strong>Bonne réponse :</strong> ${reponse.reponseCorrect}`;
+
+    divResult.appendChild(questionResult);
+    divResult.appendChild(divReponsesResult);
+    divResult.appendChild(repCorretResult);
+
+    finalResult.appendChild(divResult);
+  });
 }
